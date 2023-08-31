3e8ed2cabb8628834c89b1114920e77538ac4e1b
Sachin <raj960669@gmail.com>
2023-08-31 10:47:57 +0530

    first commit

---
+++ lib/mongoose.ts
@@ -0,0 +1,26 @@
+'@ts-ignore';
+import mongoose from 'mongoose';
+
+let isConnected = false; // Variable to track the connection status
+
+export const connectToDB = async () => {
+  // Set strict query mode for Mongoose to prevent unknown field queries.
+  mongoose.set('strictQuery', true);
+
+  if (!process.env.MONGO_URL) return console.log('Missing MongoDB URL');
+
+  // If the connection is already established, return without creating a new connection.
+  if (isConnected) {
+    console.log('MongoDB connection already established');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    isConnected = true; // Set the connection status to true
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.log(error);
+  }
+};
